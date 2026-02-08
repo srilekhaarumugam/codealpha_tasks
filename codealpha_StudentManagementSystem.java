9c82a03a84bdcef51c5ea3019dc3b2d98649f9c4
srilekhaarumugam <srilekhaarumugam4@gmail.com>
2026-02-08 19:50:23 +0530

    Add files via upload

---
+++ codealpha_StudentManagementSystem.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+    int age;
+
+    Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Age: " + age);
+    }
+}
+
+public class StudentManagementSystem {
+
+    static ArrayList<Student> students = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int choice;
+
+        do {
+            System.out.println("\n--- Student Management System ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+
+            choice = sc.nextInt();
+            sc.nextLine(); // clear buffer
+
+            switch (choice) {
+                case 1:
+                    addStudent();
+                    break;
+                case 2:
+                    viewStudents();
+                    break;
+                case 3:
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+        } while (choice != 3);
+    }
+
+    static void addStudent() {
+        System.out.print("Enter ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = sc.nextInt();
+
+        students.add(new Student(id, name, age));
+        System.out.println("Student added successfully!");
+    }
+
+    static void viewStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students available.");
+            return;
+        }
+
+        System.out.println("\nStudent List:");
+        for (Student s : students) {
+            s.display();
+        }
+    }
+}
